b83c74286c8103356577ecfc93b190ada48a43f4
jamesonatic <jamesonatic@users.noreply.github.com>
2021-04-06 14:29:11 +0200

    #4 Adding a service, removed a late code

--- ContosoCrafts.Website/Startup.cs
+++ ContosoCrafts.Website/Startup.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.Website
 {
@@ -24,6 +25,7 @@ namespace ContosoCrafts.Website
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddTransient<JsonFileProductService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
