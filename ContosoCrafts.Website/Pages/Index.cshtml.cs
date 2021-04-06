72820c62c07eac2cd82d5c39fbc1f22e344e25ad
jamesonatic <jamesonatic@users.noreply.github.com>
2021-04-06 14:38:51 +0200

    #5 Add data to a Razor page

--- ContosoCrafts.Website/Pages/Index.cshtml.cs
+++ ContosoCrafts.Website/Pages/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ContosoCrafts.Website.Models;
+using ContosoCrafts.WebSite.Services;
 
 namespace ContosoCrafts.Website.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-
-        public IndexModel(ILogger<IndexModel> logger)
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
