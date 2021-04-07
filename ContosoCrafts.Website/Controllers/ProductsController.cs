1539c9c5fa3986f3b4cf3871871b20ca6a04f113
jamesonatic <jamesonatic@users.noreply.github.com>
2021-04-07 19:40:08 +0200

    #8 Added rating function (extended API & service)

--- ContosoCrafts.Website/Controllers/ProductsController.cs
+++ ContosoCrafts.Website/Controllers/ProductsController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using ContosoCrafts.Website.Models;
 using ContosoCrafts.WebSite.Services;
+using Microsoft.Extensions.Primitives;
 
 namespace ContosoCrafts.Website.Controllers
 {
@@ -24,6 +25,17 @@ namespace ContosoCrafts.Website.Controllers
         public IEnumerable<Product> Get()
         {
             return ProductService.GetProducts();
+        }
+
+        // https://localhost:44321/products/Rate?ProductId=jenlooper-light&Rating=4
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+            [FromQuery] string ProductId, 
+            [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId,Rating);
+            return Ok();
         }
     }
 }
